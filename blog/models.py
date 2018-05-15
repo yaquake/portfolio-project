586864f3f51200cd14f6380a4f0a9ef331db72f1
yaquake <ivan@programmer.net>
2018-05-16 11:18:24 +1200

    Jobs ip and running

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to="image/")
+    text = models.TextField(max_length=2000)
