0d8ac97a80f803b8965954fac25aa48235fb57a2
yaquake <ivan@programmer.net>
2018-05-16 19:46:48 +1200

    Finished personal blog

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,13 @@ class Blog(models.Model):
     pub_date = models.DateTimeField()
     image = models.ImageField(upload_to="images/")
     body = models.TextField()
+
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
