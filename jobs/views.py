3a0d93b786b2dbdde94521e03e16f5880032d869
Ivan <ivan@programmer.net>
2019-02-08 21:44:59 +1300

    rubbish cleaned; picture updated

--- jobs/views.py
+++ jobs/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 
 from .models import Job
 
-# Create your views here.
+
 def home(request):
-    jobs = Job.objects
-    return render(request, 'jobs/home.html', {'jobs':jobs})
+    jobs = Job.objects.all()
+    return render(request, 'jobs/home.html', {'jobs': jobs})
+
